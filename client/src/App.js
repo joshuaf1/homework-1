b63693bbe9dcf26246d5c1ebadffd58fb6f9e6ba
Joshua Fitzhugh <joshuaf1@umbc.edu>
2022-03-04 20:48:28 -0500

    Release Version

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,144 @@
 import React, { useState, useEffect } from 'react'
+import "./App.css";
+import Axios from "axios";
 
 function App() {
 
-  const [data, setData] = useState([{}])
+  const [name, setName] = useState("");
+  const [id, setID] = useState(0);
+  const [points, setPoints] = useState(0);
+
+  const [targetID, setTargetID] = useState(0);
+
+  const [users, setUsers] = useState([]);
+
+  const createUser = () => {
+    Axios.post("http://localhost:5000/api/create", {
+      name: name,
+      id: id,
+      points: points
+    }).then(
+      Axios.get("http://localhost:5000/api/read").then((response) => {
+        setUsers(response.data.json_list);
+        window.location.reload();
+      })
+    )
+  };
 
   useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
+    Axios.get("http://localhost:5000/api/read").then((response) => {
+      setUsers(response.data.json_list);
+    });
+  }, []);
+
+  const updateUser = () => {
+    Axios.post("http://localhost:5000/api/update", {
+      targetID: targetID,
+      name: name,
+      id: id,
+      points: points
+    }).then(
+      Axios.get("http://localhost:5000/api/read").then((response) => {
+        setUsers(response.data.json_list);
+        window.location.reload();
+      })
     )
-  }, [])
+  };
+
+  const deleteUser = () => {
+    Axios.post('http://localhost:5000/api/delete', {
+      targetID: targetID
+    }).then(
+      Axios.get("http://localhost:5000/api/read").then((response) => {
+        setUsers(response.data.json_list);
+        window.location.reload();
+      })
+    )
+  };
 
   return (
-    <div>
+    <div id="App">
 
-      {(typeof data.members === 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        data.members.map((member, i) => (
-          <p key={i}>{member}</p>
-        ))
-      )}
+      <div id="title">
+        CRUD APPLICATION
+      </div>
 
+      <div className="form">
+        <label>Name: </label>
+        <input
+          type="text"
+          name="name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <label>ID: </label>
+        <input
+          type="number"
+          name="id"
+          onChange={(e) => {
+            setID(e.target.value);
+          }}
+        />
+        <label>Points: </label>
+        <input
+          type="number"
+          name="points"
+          onChange={(e) => {
+            setPoints(e.target.value);
+          }}
+        />
+        <button onClick={createUser}>Create</button>
+      </div>
+
+      <div className="form">
+        <label>Target ID: </label>
+        <input
+          type="number"
+          name="targetID"
+          onChange={(e) => {
+            setTargetID(e.target.value);
+          }}
+        />
+        <button onClick={() => { updateUser() }}>Update</button>
+        <button onClick={() => { deleteUser() }}> Delete</button>
+      </div>
+
+      <div id="tableOfUsers">
+        <div id="tableCaption">
+          List of Users in Database
+        </div>
+        <div id="tableHeader">
+          <div className="tableHeaderCell">
+            Name
+          </div>
+          <div className="tableHeaderCell">
+            ID
+          </div>
+          <div className="tableHeaderCell">
+            Points
+          </div>
+        </div>
+        <div id="tableBody">
+          {users.map((val) => {
+            return (
+              <div className={"tableRow"} key={val.id}>
+                <div className="tableCell">
+                  {val.name}
+                </div>
+                <div className="tableCell">
+                  {val.id}
+                </div>
+                <div className="tableCell">
+                  {val.points}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
---
+++ flask-server/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, request 
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+class Users(db.Model):
+    name = db.Column(db.String(200), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    points = db.Column(db.Integer, nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'name'   : self.name,
+            'id'     : self.id,
+            'points' : self.points
+        }
+
+@app.route('/')
+def index():
+    return "Why are snooping around my backend? ( ͡° ͜ʖ ͡°)"
+
+@app.route('/api/create', methods=['POST'])
+def create():
+    data = request.get_json()
+    create_name = data.get('name')
+    create_id = data.get('id')
+    create_points = data.get('points')
+    create_user = Users(name = create_name, id = create_id, points = create_points)
+    db.session.add(create_user)
+    db.session.commit()
+    return "User succesfully created."
+
+@app.route('/api/read', methods=['GET'])
+def read():
+    users = Users.query.all()
+    return jsonify(json_list = [i.serialize for i in users])
+
+@app.route('/api/delete', methods=['POST'])
+def delete():
+    data = request.get_json()
+    del_user = Users.query.get_or_404(data.get('targetID'))
+    db.session.delete(del_user)
+    db.session.commit()
+    return "User successfully delete."
+
+@app.route('/api/update', methods=['POST'])
+def update():
+    data = request.get_json()
+    user = Users.query.get_or_404(data.get('targetID'))
+    user.name = data.get('name')
+    user.id = data.get('id')
+    user.points = data.get('points')
+    db.session.commit()
+    return "User successfully updated."
+
+if __name__ == "__main__":
+    app.run(debug=True)
